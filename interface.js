d5313db2500d2d5790c3f9a38ccbe1e5a5915dd2
Cameron Tacklind <cameron@tacklind.com>
2015-10-31 17:06:32 -0700

    Minimal implementation

--- interface.js
+++ interface.js
@@ -1,2 +1,14 @@
+'use strict';
 
-  console.log('hi!');
+var XLN = require('./XLN.js');
+
+
+var x = new XLN({host: '192.168.1.166'}, function() {
+  console.log('done');
+});
+
+setInterval(function () {
+  x.readStatus(function(msg) {
+    console.log(msg);
+  });
+}, 1000);
