ea413512a3eea3e2fcf1df1662f35f9d333eec72
Cameron Tacklind <cameron@tacklind.com>
2015-11-03 02:28:53 -0800

    Use JSX. It's cleaner.

--- src/renderer/bootstrap.js
+++ src/renderer/bootstrap.js
@@ -4,4 +4,4 @@ import polyfill from 'babel/polyfill';
 import React from 'react';
 import {Main} from './components/main';
 
-React.render(React.createElement(Main), document.body);
+React.render(<Main />, document.body);
