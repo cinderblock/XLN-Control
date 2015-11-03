99205d848b0271d83ad62514b53a1480b3df7e32
Cameron Tacklind <cameron@tacklind.com>
2015-11-03 00:36:22 -0800

    Cleaner body

--- src/renderer/bootstrap.js
+++ src/renderer/bootstrap.js
@@ -4,4 +4,4 @@ import polyfill from 'babel/polyfill';
 import React from 'react';
 import {Main} from './components/main';
 
-React.render(React.createElement(Main), document.getElementById('app'));
+React.render(React.createElement(Main), document.body);
