13bee2fe94e9ce361b5febce0b4bd809ce170a59
Cameron Tacklind <cameron@tacklind.com>
2015-11-11 00:09:32 -0800

    Ugly header

--- app/containers/MainPageContainer.jsx
+++ app/containers/MainPageContainer.jsx
@@ -11,7 +11,6 @@ export default class MainPageContainer extends React.Component {
   render() {
     return (
       <div>
-        <h2>XLN6024 Control</h2>
         <XLN host="192.168.1.167" />
       </div>
     );
