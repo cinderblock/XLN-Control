a6a0e5a24fbf88e71752a58b1fe114c91edd011c
Cameron Tacklind <cameron@tacklind.com>
2015-11-11 00:21:59 -0800

    Example FontAwesome import / usage

--- app/containers/MainPageContainer.jsx
+++ app/containers/MainPageContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import XLN from '../components/XLN';
+import FA from 'react-fontawesome';
 
 export default class MainPageContainer extends React.Component {
 
@@ -12,6 +13,7 @@ export default class MainPageContainer extends React.Component {
     return (
       <div>
         <XLN host="192.168.1.167" />
+        <FA name='rocket' />
       </div>
     );
   }
