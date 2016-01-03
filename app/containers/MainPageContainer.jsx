80504b0b2def2e5b8e1edd451313606f5271501b
Cameron Tacklind <cameron@tacklind.com>
2016-01-03 03:21:08 -0800

    Don't need the rocket example anymore

--- app/containers/MainPageContainer.jsx
+++ app/containers/MainPageContainer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
 import XLN from '../components/XLN';
-import FA from 'react-fontawesome';
 
 export default class MainPageContainer extends React.Component {
 
@@ -13,7 +12,6 @@ export default class MainPageContainer extends React.Component {
     return (
       <div>
         <XLN host="192.168.1.167" />
-        <FA name='rocket' />
       </div>
     );
   }
